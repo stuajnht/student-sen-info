2cb1083745dbed53a88e42b6906d82c9931e0fd3
hartj <hartj@sgse.co.uk>
2015-09-21 18:47:41 -0400

    Added details.php page to hold a template for student details

---
+++ details.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of student-sen-info.
+ *
+ * student-sen-info is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * student-sen-info is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with student-sen-info.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Jonathan Hart
+ */
+
+/*
+ * The individual student details template page, which is filled in via AJAX
+ *
+ * This file holds the main template for the details of the student, with most
+ * of the information being filled in through AJAX calls
+ */
+?>
