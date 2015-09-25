0a883321bf9b3052ca2b36fd805c38973c981648
hartj <hartj@sgse.co.uk>
2015-09-25 04:08:30 -0400

    Added code to display the search form when the search button is pressed

--- footer.php
+++ footer.php
@@ -28,5 +28,36 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/animsition/3.5.2/js/jquery.animsition.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/simplemodal/1.4.4/jquery.simplemodal.min.js"></script>
 <script>new WOW().init();</script>
+<script>
+	// Controlling the search form being shown
+	$(document).ready( "#button--show-search" ).click(function() {
+		$('.mdl-layout__content').addClass('animated fadeOut');
+		$('.mdl-layout__content').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+			$('.mdl-layout__content').empty();
+			$('.mdl-layout__content').removeClass('animated fadeOut');
+			
+			if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+				xmlhttp=new XMLHttpRequest();
+			} else {// code for IE6, IE5
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.onreadystatechange=function() {
+				// Seeing what was returned
+				if (xmlhttp.readyState==4 && xmlhttp.status!=200) {
+					// Any HTTP error results in an error message being shown
+					alert('Unable to load the search form. Please try again.');
+				}
+				if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+					// Displaying the results in the div
+					result = xmlhttp.responseText;
+					//alert(result);
+					$('.mdl-layout__content').html(result);
+				}
+			}
+			xmlhttp.open("GET","search.php",true);
+			xmlhttp.send();
+		});
+	});
+</script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -46,15 +46,9 @@
         <div class="mdl-layout__header-row">
           <span class="mdl-layout-title">SEN Info</span>
           <div class="mdl-layout-spacer"></div>
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-            <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
-              <i class="material-icons">search</i>
-            </label>
-            <div class="mdl-textfield__expandable-holder">
-              <input class="mdl-textfield__input" type="text" id="search" />
-              <label class="mdl-textfield__label" for="search">Student name&hellip;</label>
-            </div>
-          </div>
+          <button class="mdl-button mdl-js-button mdl-button--icon" id="button--show-search">
+			  <i class="material-icons">search</i>
+			</button>
           <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
             <i class="material-icons">more_vert</i>
           </button>
